35a498d062e5a6f409a807570c1bb45898538600
distrolinux-th <linuxdistro111@gmail.com>
2026-07-09 15:31:59 +0700

    Create install.sh
    Add bootstrap installer

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo "================================="
+echo "Termux-X11 Debian Installer"
+echo "================================="
+
+echo "Installer coming soon..."
