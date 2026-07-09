beee7d363594fc9aecc3394d3f86364fef47fab6
Linux Launch <linuxdistro111@gmail.com>
2026-07-09 20:52:43 +0700

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-
-echo "================================="
-echo "Termux-X11 Debian Installer"
-echo "================================="
-
-echo "Installer coming soon..."
