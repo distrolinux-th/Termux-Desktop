d11087751823d1119506747c33a707bb2b6f399d
distrolinux-th <linuxdistro111@gmail.com>
2026-07-09 15:43:21 +0700

    Create main.sh
    Add main menu

---
+++ main.sh
@@ -0,0 +1,11 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+clear
+
+echo "================================="
+echo " Termux-X11 Debian Installer"
+echo "================================="
+
+echo
+echo "1) Install Everything"
+echo "2) Exit"
