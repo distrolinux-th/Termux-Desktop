18c9bb8b510d7e17118baefccaeb31e554ca9ea5
distrolinux-th <linuxdistro111@gmail.com>
2026-07-09 15:55:02 +0700

    Update common.sh
    Add common functions

--- scripts/common.sh
+++ scripts/common.sh
@@ -27,6 +27,14 @@ error() {
     exit 1
 }
 
+check_termux() {
+    [ -d "/data/data/com.termux" ] || error "Please run this script inside Termux."
+}
+
+check_command() {
+    command -v "$1" >/dev/null 2>&1 || error "$1 is not installed."
+}
+
 check_internet() {
     if ping -c 1 8.8.8.8 >/dev/null 2>&1; then
         success "Internet connection detected."
