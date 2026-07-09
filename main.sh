73023c67a155ee96a879115fe24d447d9f3ea786
distrolinux-th <linuxdistro111@gmail.com>
2026-07-09 17:00:53 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -8,17 +8,23 @@ while true; do
     echo "========================================"
     echo
     echo "1) Install Everything"
-    echo "2) Exit"
+    echo "2) Install Termux:X11"
+    echo "3) Exit"
     echo
-
+    
     read -p "Select: " choice
 
     case "$choice" in
         1)
             bash scripts/00-check.sh
             bash scripts/01-termux.sh
+            bash scripts/02-termux-x11.sh
             ;;
-        2)
+        2)  
+            bash scripts/02-termux-x11.sh
+            exit 0
+            ;;
+        3)
             exit 0
             ;;
         *)
