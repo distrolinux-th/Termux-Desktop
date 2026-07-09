d44c039c286d74d237711c68adadad4466d2173d
distrolinux-th <linuxdistro111@gmail.com>
2026-07-09 16:42:58 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,11 +1,29 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-clear
+while true; do
+    clear
 
-echo "================================="
-echo " Termux-X11 Debian Installer"
-echo "================================="
+    echo "========================================"
+    echo "     Termux Desktop Installer"
+    echo "========================================"
+    echo
+    echo "1) Install Everything"
+    echo "2) Exit"
+    echo
 
-echo
-echo "1) Install Everything"
-echo "2) Exit"
+    read -p "Select: " choice
+
+    case "$choice" in
+        1)
+            bash scripts/00-check.sh
+            bash scripts/01-termux.sh
+            ;;
+        2)
+            exit 0
+            ;;
+        *)
+            echo "Invalid option!"
+            sleep 2
+            ;;
+    esac
+done
